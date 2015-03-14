0e87dcd520941f11fcf4130c21a7e1846739bdb0
Sergey Polischuk <spolischook@gmail.com>
2015-03-14 15:14:38 +0700

    Added domain option as required options to all app/console commands

---
+++ EventListener/DomainListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GeekHub\DomainRoutingBundle\EventListener;
+
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\InputOption;
+
+class DomainListener
+{
+    public function onConsoleCommand(ConsoleCommandEvent $event)
+    {
+        $inputDefinition = $event->getCommand()->getApplication()->getDefinition();
+
+        // add the option to the application's input definition
+        $inputDefinition->addOption(
+            new InputOption('domain', 'd', InputOption::VALUE_REQUIRED, 'Domain name for include routing', null)
+        );
+    }
+}
