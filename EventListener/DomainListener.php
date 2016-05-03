6122f7cc1cb8a9ef8a92de4fbf1c912a9e852fd2
Serhii Polishchuk <spolishchuk@magecore.com>
2016-05-03 16:02:36 +0300

    Update behavior for symfony >=2.8

--- EventListener/DomainListener.php
+++ EventListener/DomainListener.php
@@ -7,13 +7,18 @@ use Symfony\Component\Console\Input\InputOption;
 
 class DomainListener
 {
+    /**
+     * @param ConsoleCommandEvent $event
+     */
     public function onConsoleCommand(ConsoleCommandEvent $event)
     {
-        $inputDefinition = $event->getCommand()->getApplication()->getDefinition();
+        $definition = $event->getCommand()->getDefinition();
+        $input = $event->getInput();
 
         // add the option to the application's input definition
-        $inputDefinition->addOption(
+        $definition->addOption(
             new InputOption('hostname', null, InputOption::VALUE_REQUIRED, 'Domain name for include routing', null)
         );
+        $input->bind($definition);
     }
 }
